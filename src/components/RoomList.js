8bf6e64a95d9ec060d9f9ae1c794821d57dcbe79
Melissa Riggs <melissarocksoc@gmail.com>
2018-07-12 17:34:08 -0700

    first step of create chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,29 +3,46 @@ import React, { Component } from 'react';
 export class RoomList extends Component{
     constructor(props){
         super(props);
+        this.roomsRef = this.props.firebase.database().ref('rooms');
         this.state = {
             rooms: []
           };
-          this.roomsRef = this.props.firebase.database().ref('rooms');
     }
 
     componentDidMount() {
         this.roomsRef.on('child_added', snapshot => {
             const room = snapshot.val();
             room.key = snapshot.key;
-            this.setState({ rooms: this.state.rooms.concat( room ) })
+            this.setState({ rooms: this.state.rooms.concat( room ), 
+            new_room: ''
+          });
         });
+      }
+
+      newRoom(e) {
+          e.preventDefault();
+            this.roomsRef.push({ name: this.newRoomRef });
+      }
+
+      pushNewRoom(e) {
+          this.setState( { new_room: e.target.value })
+          this.newRoomRef = this.state.new_room;
       }
 
       render() {
         return(
-            <div className='app'>
+            <div className='navigation'>
             <div className='sidenav'>
             <h3>Bloc Chat</h3>
             {
-this.state.rooms.map((list, index) => <p className='rooms' key={index}>{list.name}</p>
+                this.state.rooms.map((list, index) => <p className='rooms' key={index}>{list.name}</p>
 )}
             </div>
+
+            <form onSubmit={ (e) => this.newRoom(e) }>
+                <input type="text" value={this.state.newRoomRef} onChange={(e) => this.pushNewRoom(e) }/>
+                <input type="submit" value="Create New Room"  />
+            </form>
             </div>
         );
     }
